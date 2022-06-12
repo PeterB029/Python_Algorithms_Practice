87ad654fea6089cd0557dbf619781fa2b1ed461f
Peter Bui <peter.bui029@gmail.com>
2022-06-12 14:09:51 -0400

    reviewed max and min functions with sequences

---
+++ sequence_operations/maximum.py
@@ -0,0 +1,21 @@
+#Maximum - Find teh maximum item in a sequence lexicographically.
+#Alpha OR Numerical, NOT MIXED
+
+#max([sequence]) ⇒ returns the maximum item in [sequence]
+
+#string
+x = 'llama'
+print(max(x))
+#output: m
+
+#list
+y = ['happy', 'llama', 'explorer']
+print(max(y))
+#output: llama
+#compares the first letter alphabetically
+
+#tuple
+z = ('Itadori', 'Fushigiro', 'Nobara', 'Gojo')
+print(max(z))
+#output: Nobara
+#compares the first letter alphabetically
