2733fb390511c4779b16b241fcf6ae9df3fa6c6f
Peter Bui <peter.bui029@gmail.com>
2022-06-12 13:53:31 -0400

    reviewed python multiply

---
+++ sequence_operations/multiplying.py
@@ -0,0 +1,16 @@
+#Multiplying - multiply a sequence using '*'
+
+#string
+x = 'cat' * 3
+print(x)
+#output: catcatcat
+
+#list
+y = [7, 5] * 2
+print(y)
+#output: [7, 5, 7, 5]
+
+#tuple
+z = (2, 4) * 3
+print(z)
+#output: (2, 4, 2, 4, 2, 4)
