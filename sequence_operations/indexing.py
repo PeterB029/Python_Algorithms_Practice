da15262bb07c965038bb41404f3c1cc47c1546b6
Peter Bui <peter.bui029@gmail.com>
2022-06-12 13:42:23 -0400

    initial commit

---
+++ sequence_operations/indexing.py
@@ -0,0 +1,20 @@
+#string
+x = 'llama'
+print(x[3]) 
+#output: 'm'
+#l l a m a
+#0 1 2 3 4
+
+#list
+x = ['llama','cat','duck']
+print(x[1])
+#output: 'cat'
+#['llama','cat','duck']
+#[    0  ,  1  ,   2  ]
+
+#tuple
+x = ('llama', 'cat', 'dog', 'duck')
+print(x[0])
+#output: 'llama'
+#('llama', 'cat', 'dog', 'duck')
+#(   0   ,   1  ,   2  ,    3  )
