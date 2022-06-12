f0b72b5544950a257a9087090394cfa3c29c663a
Peter Bui <peter.bui029@gmail.com>
2022-06-12 14:15:18 -0400

    reviewed sorting a sequence with sorted()

---
+++ sequence_operations/sorting.py
@@ -0,0 +1,17 @@
+#Sorting - returns a NEW list of items in sorted order.
+#Does NOT change the original list!
+
+#string
+x = 'bug'
+print(sorted(x))
+#output: ['b', 'g', 'u']
+
+#list
+y = ['pig', 'cow', 'horse']
+print(sorted(y))
+#output: ['cow', 'horse', 'pig']
+
+#tuple
+z = ('Itadori', 'Fushigiro', 'Nobara', 'Gojo')
+print(sorted(z))
+#output: ['Fushigiro', 'Gojo', 'Itadori', 'Nobara']
