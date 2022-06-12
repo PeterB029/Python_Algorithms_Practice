1b1eac624b7d5a03824ddc2ce4068d946aa36a17
Peter Bui <peter.bui029@gmail.com>
2022-06-12 13:50:55 -0400

    reviewed adding & concatenating

---
+++ sequence_operations/adding_concatenating.py
@@ -0,0 +1,16 @@
+#Adding/Concatenating - Combine 2 sequences of the same type by using '+'
+
+#string
+x = 'Llama' + 'Explorer'
+print(x)
+#output: LlamaExplorer
+
+#list
+y = ['happy','llama'] + ['explorer']
+print(y)
+#['happy', 'llama', 'explorer']
+
+#tuple
+z = ('Itadori', 'Fushigiro', 'Nobara') + ('Gojo',)
+print(z)
+#('Itadori', 'Fushigiro', 'Nobara', 'Gojo')
