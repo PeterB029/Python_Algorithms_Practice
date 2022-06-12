67e29e2e01aac6930f3aa314067e8c7d80361733
Peter Bui <peter.bui029@gmail.com>
2022-06-12 14:01:44 -0400

    reviewed how to get the length of a sequence using len()

---
+++ sequence_operations/number_of_items.py
@@ -0,0 +1,17 @@
+#Number of Items - Count the number of items in a squence 
+#len([sequence]) ⇒  length of [sequence]
+
+#string
+x = 'llama'
+print(len(x))
+#output: 5
+
+#list
+y = ['happy', 'llama', 'explorer']
+print(len(y))
+#output: 3
+
+#tuple
+z = ('Itadori', 'Fushigiro', 'Nobara', 'Gojo')
+print(len(z))
+#output: 4
