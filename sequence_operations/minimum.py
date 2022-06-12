672827b0254f69dcf533d437561766a2e413b8a6
Peter Bui <peter.bui029@gmail.com>
2022-06-12 14:06:16 -0400

    reviewed the min() function with sequences

---
+++ sequence_operations/minimum.py
@@ -0,0 +1,21 @@
+#Minimum - Find the minimum item in a sequence lexicographically.
+#lpha OR Numerical, NOT MIXED
+
+#min([sequence]) ⇒ returns the minimun item in [sequence]
+
+#string
+x = 'llama'
+print(min(x))
+#output: a
+
+#list
+y = ['happy', 'llama', 'explorer']
+print(min(y))
+#output: explorer
+#compares the first letter alphabetically
+
+#tuple
+z = ('Itadori', 'Fushigiro', 'Nobara', 'Gojo')
+print(min(z))
+#output: Fushigiro
+#compares the first letter alphabetically
