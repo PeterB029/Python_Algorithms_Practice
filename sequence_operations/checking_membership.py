ca2d23a3181ba18f0a1a9070914f3b5fb1cdee5f
Peter Bui <peter.bui029@gmail.com>
2022-06-12 13:55:51 -0400

    reviewed checking membership in a sequence

---
+++ sequence_operations/checking_membership.py
@@ -0,0 +1,19 @@
+# Checking Membership - Test whether an item is or is not in a sequence.
+
+#string
+x = 'llama'
+print('m' in x)
+#output: True
+#m is found in llama, so the statement is true
+
+#list
+y = ['happy', 'llama', 'explorer']
+print('happy' not in y)
+#output: false 
+#happy is found in y, so the statement is false
+
+#tuple
+z = ('Itadori', 'Fushigiro', 'Nobara', 'Gojo')
+print('Fushigiro' in z)
+#output: true
+#Fushigiro is found in z, so the statement is true
