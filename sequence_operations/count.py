346b7a8d45c829ebc40f6515e4697e9b7b950cb7
Peter Bui <peter.bui029@gmail.com>
2022-06-12 14:19:25 -0400

    reviewed count() function with sequences

---
+++ sequence_operations/count.py
@@ -0,0 +1,19 @@
+# Count - Returns the count of an item in sequence.
+
+# count(item) ⇒ returns how many times item shows up in sequence.
+
+#string
+x = 'llama'
+print(x.count('l'))
+#output: 2 
+#there are 2 'l' in 'llama'
+
+#list
+y = ['happy', 'llama', 'explorer']
+print(y.count('explorer'))
+#output: 1
+
+#tuple
+z = ('Itadori', 'Fushigiro', 'Nobara', 'Gojo')
+print(z.count('Gojo'))
+#output: 1
