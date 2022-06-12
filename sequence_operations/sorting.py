d70e647bd530dcbdd941e1714f8066087c1f0659
Peter Bui <peter.bui029@gmail.com>
2022-06-12 14:18:02 -0400

    added to sorting.py- example of sort by second letter

--- sequence_operations/sorting.py
+++ sequence_operations/sorting.py
@@ -15,3 +15,14 @@ print(sorted(y))
 z = ('Itadori', 'Fushigiro', 'Nobara', 'Gojo')
 print(sorted(z))
 #output: ['Fushigiro', 'Gojo', 'Itadori', 'Nobara']
+
+# NOTE: The results provided are a LIST regardless of what it was originally (string, tuple). The original is untouched and a new list is returned. 
+
+
+# Sorting by 2nd Letter
+# Add a key parameter and a lambda function to return the second character.
+# (The word key here is a defined parameter name, k is an arbitrary variable name).
+z = ('Itadori', 'Fushigiro', 'Nobara', 'Gojo')
+print(sorted(z, key=lambda k: k[1]))
+#output: ['Nobara', 'Gojo', 'Itadori', 'Fushigiro']
+#for each item k, we are comparing the second letter k[1] (index 1, not 0)
