876e677dae0087994f3cb63fd4fb5130f65998e5
Peter Bui <peter.bui029@gmail.com>
2022-06-16 13:17:54 -0400

    Reviewed sets: poping items and deleting all timesin a set

---
+++ Notes/4_sets/delete.py
@@ -0,0 +1,5 @@
+#Delete all items from a set
+x = {3, 8, 5}
+x.clear()
+print(x)
+#output: set()
---
+++ Notes/4_sets/pop.py
@@ -0,0 +1,4 @@
+# Pop and Item from a set (It will be a random item since sets are NOT ordered!)
+x = {3, 8, 5}
+print(x.pop(), x)
+#output: 8 {3, 5}
